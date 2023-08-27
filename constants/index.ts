1f6e2204cdb1cfa416b2fcab863625be90e4f888
Teddy Wagner <nicholas.e.wagner@vanderbilt.edu>
2023-08-27 11:58:53 -0400

    About page

---
+++ constants/index.ts
@@ -0,0 +1,24 @@
+export const entertainment = {
+  key: "Entertainment",
+  value: [
+    "Jet Ski",
+    "Wake Boards",
+    "Floats",
+    'The "In Touch" Dingy',
+    "Snorkeling Equipment",
+    "Fishing Equipment",
+    "Wi-Fi",
+  ],
+};
+
+export const interior = {
+  key: "Interior",
+  value:
+    "There is plenty of room as it features four spacious rooms. A Master Suite with a large bed-sitting under a panoramic mirror and comfortable seating. A VIP Queen Suite with a large bed and plenty of space and a window view. And two twin cabins with two beds in each room.",
+};
+
+export const dining = {
+  key: "Dining",
+  value:
+    "Located at the back of the yacht's main deck, our outdoor dining space offers a captivating culinary experience amidst the beauty of the open sea. The expansive hardwood deck is adorned with a thoughtfully set table and comfortable seating, providing an inviting atmosphere for savoring exquisite meals while being caressed by the gentle sea breeze.",
+};
