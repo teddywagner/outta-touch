ba60b28b12e2d4a944d15367f72ce3813e0ca012
Teddy Wagner <nicholas.e.wagner@vanderbilt.edu>
2023-08-27 13:29:29 -0400

    Make Header component reactive

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,30 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { Instagram, PhoneEnabled, EmailOutlined } from "@mui/icons-material";
+import {
+  Instagram,
+  PhoneEnabled,
+  EmailOutlined,
+  Menu as MenuIcon,
+  InfoOutlined,
+  FilterOutlined,
+  MessageOutlined,
+} from "@mui/icons-material";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
 const Header = () => {
   return (
-    <header className="flex-between bg-gradient-radial from-faded-white w-full absolute padding-y md:padding-x text-white h-[7rem]">
-      <div className="flex flex-1 gap-5">
-        <Link href="/about" className="hover:underline">
+    <header className="flex sm:justify-between items-center bg-gradient-radial from-faded-white w-full absolute padding-y padding-x text-white h-[7rem]">
+      <div className="flex sm:flex-1 gap-5">
+        <Link href="/about" className="hover:underline hidden sm:flex">
           About us
         </Link>
-        <Link href="/gallery" className="hover:underline">
+        <Link href="/gallery" className="hover:underline hidden sm:flex">
           Gallery
         </Link>
       </div>
-      <Link href="/">
+      <Link href="/" className="flex justify-self-start">
         <Image
           src="/outoftouch.png"
           alt="Outta Touch logo"
@@ -23,11 +33,60 @@ const Header = () => {
           className="object-contain"
         />
       </Link>
-      <div className="flex flex-1 justify-end gap-3">
-        Contact us
+      <div className="sm:flex flex-1 justify-end gap-3 hidden">
         <Instagram />
         <PhoneEnabled />
         <EmailOutlined />
+      </div>
+      <div className="flex flex-1 justify-end sm:hidden">
+        <Menu as="div" className="relative inline-block text-left">
+          <Menu.Button>
+            <MenuIcon />
+          </Menu.Button>
+          <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-black">
+            <div className="flex flex-col px-1 py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`rounded-md px-1 py-1 ${
+                      active && "bg-deep-blue text-white"
+                    }`}
+                    href="/about"
+                  >
+                    <InfoOutlined />
+                    <span className="ml-1">About us</span>
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`rounded-md px-1 py-1 ${
+                      active && "bg-deep-blue text-white"
+                    }`}
+                    href="/gallery"
+                  >
+                    <FilterOutlined />
+                    <span className="ml-1">Gallery</span>
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`rounded-md px-1 py-1 ${
+                      active && "bg-deep-blue text-white"
+                    }`}
+                    href="/contact"
+                  >
+                    <MessageOutlined />
+                    <span className="ml-1">Contact us</span>
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Menu>
       </div>
     </header>
   );
