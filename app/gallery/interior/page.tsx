1e8e13ff3a8de43c3a7b77b0e733856fdd70015e
Teddy Wagner <nicholas.e.wagner@vanderbilt.edu>
2023-09-05 21:03:35 -0400

    Force static pages to be dynamic for supabase

--- app/gallery/interior/page.tsx
+++ app/gallery/interior/page.tsx
@@ -2,6 +2,8 @@ import { Gallery } from "@/components";
 import { getImages } from "../page";
 import { FolderType } from "@/types";
 
+export const dynamic = "force-dynamic";
+
 const page = async () => {
   const images = await getImages();
   return (
